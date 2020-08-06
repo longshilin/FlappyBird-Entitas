43cf3da9062e53650394561cf567931c7a7a4e61
José Villegas <villegasjose.gg@gmail.com>
2020-08-06 17:40:11 -0400

    Pipe movement

--- Assets/Features/Pipe/PipeMovementSystem.cs
+++ Assets/Features/Pipe/PipeMovementSystem.cs
@@ -1,46 +1,30 @@
-﻿using System.Collections.Generic;
-using Entitas;
+﻿using Entitas;
 using UnityEngine;
 
 namespace Pipe
 {
-    public class PipeMovementSystem : ReactiveSystem<GameEntity>
+    public class PipeMovementSystem : IExecuteSystem
     {
         readonly Contexts _contexts;
 
-        public PipeMovementSystem(Contexts contexts) : base(contexts.game)
+        public PipeMovementSystem(Contexts contexts)
         {
             _contexts = contexts;
         }
 
-        public PipeMovementSystem(IContext<GameEntity> context) : base(context)
-        {
-        }
-
-        public PipeMovementSystem(ICollector<GameEntity> collector) : base(collector)
-        {
-        }
-
-        protected override ICollector<GameEntity> GetTrigger(IContext<GameEntity> context)
-        {
-            return context.CreateCollector(GameMatcher.Pipe);
-        }
-
-        protected override bool Filter(GameEntity entity)
-        {
-            return entity.isPipe;
-        }
-
-        protected override void Execute(List<GameEntity> entities)
+        public void Execute()
         {
             var configuration = _contexts.configuration.gameConfiguration.value;
+            var group = _contexts.game.GetGroup(GameMatcher.Pipe);
 
-            foreach (var gameEntity in entities)
+            foreach (var gameEntity in group)
             {
                 if (!gameEntity.isPipe) continue;
 
                 var pos = gameEntity.position.Value;
-                gameEntity.ReplacePosition(pos + Vector3.right * Time.deltaTime * configuration.PipeMovementSpeed);
+                pos -= Vector3.right * Time.deltaTime * configuration.PipeMovementSpeed;
+                Debug.Log(pos.x);
+                gameEntity.ReplacePosition(pos);
             }
         }
     }

24bdb54546ef69667073a208e1a827f5a3cf0cb8
José Villegas <villegasjose.gg@gmail.com>
2020-08-07 16:50:36 -0400

    Ground anim

---
+++ Assets/Features/Animation/GroundAnimation.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class GroundAnimation : MonoBehaviour, IAnyGameLostListener, IAnyGameStartedListener
+{
+    private Animator _animator;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+
+        var e = Contexts.sharedInstance.game.CreateEntity();
+        e.AddAnyGameLostListener(this);
+        e.AddAnyGameStartedListener(this);
+    }
+
+    public void OnAnyGameLost(GameEntity entity)
+    {
+        _animator.enabled = false;
+    }
+
+    public void OnAnyGameStarted(GameEntity entity)
+    {
+        _animator.enabled = true;
+    }
+}
